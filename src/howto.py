3cbdaeb22901c2ed994b5988c7c7dd2b1d523089
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2013-11-14 22:56:23 +0100

    Started parsing the post

--- src/howto.py
+++ src/howto.py
@@ -1,5 +1,6 @@
 import pytumblr
 import json
+from HTMLParser import HTMLParser
 
 def exposed(obj):
     obj.exposed = True
@@ -45,16 +46,60 @@ class HowToClient(object):
             id = post_id
         )
         
-        return raw 
-        status = raw["meta"]["status"]
-        if status == 200:
-            return raw
-        elif status / 100 == 4:
-            return None
+        if 'posts' in raw:
+            # We have it !
+            return self.format_post(raw['posts'][0])
         else:
-            raise TumblrServerError(raw)
 
+            status = raw["meta"]["status"]
+            if status / 100 == 4:
+                return None
+            else:
+                raise TumblrServerError(raw)
+
+    def format_post(self, raw_post):
+        if self.is_howto(raw_post):
+
+            post = {
+                'url': raw_post['short_url'],
+            }
+
+            post['name'], post['steps'] = self.parse_body(raw_post)
+
+            return post
+        else:
+            raise PostIsNotHowtoException()
+
+    def parse_body(self, raw_post):
+        text = raw_post['caption']
+
+        if raw_post['format'] == 'html':
+            text = strip_tags(text)
+
+        return 'hello', text
+    
+    def is_howto(self, raw_post):
+        return self.tag in raw_post['tags'] and raw_post['type'] == 'photo'
+
+
+class PostIsNotHowtoException(Exception):
+    pass
 
 class TumblrServerError(Exception):
     pass
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
