b5a9ac173811dbec09854fe0ccc3ae37ea1002b6
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2013-11-14 22:31:58 +0100

    Now parsing the config file from run.py

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import sys
 import os.path
 from glob import glob
+from ConfigParser import ConfigParser
 
-SRC_DIR="src"
-VENDOR_DIR="vendor"
+SRC_DIR = "src"
+VENDOR_DIR = "vendor"
+CONFIG_FILE = "localconf.ini"
 
 # find the location of this script 
 mydir = os.path.dirname(os.path.realpath(__file__))
@@ -21,11 +23,15 @@ for library_path in glob(lib_pattern):
 # note that we insert'ed into sys.path rather than appending
 # that way we're sure that our modules are loaded first
 
+# load the config
+config_parser = ConfigParser()
+config_parser.read(os.path.join(mydir, CONFIG_FILE))
+
 # all set! Load the app and let go
 
 import cherrypy
 import howto
 
-server = howto.Server()
+server = howto.Server(config_parser._sections)
 cherrypy.quickstart(server)
 
